008838eaac704a99c27df1b1014a528a9f04a5b1
Igor <146733542+Igorgvozdomet@users.noreply.github.com>
2026-06-30 15:04:12 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,46 @@
+import flet as ft
+import requests
+
+def main(page: ft.Page):
+    page.title = "Погода"
+    page.theme_mode = 'dark'
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+
+
+    user_data = ft.TextField(label = "Введите город", width = 400)
+    weather_data = ft.Text('')
+
+    def get_info(e):
+        if len(user_data.value) < 2:
+            return
+        
+        API = 'ваш_ключ_здесь'  # замените перед запуском
+        URL = f'https://api.openweathermap.org/data/2.5/weather?q={user_data.value}&appid={API}&units=metric'
+        res = requests.get(URL).json()
+        temp = res['main']['temp']
+        weather_data.value='Погода: ' + str(temp)
+        page.update()
+        
+
+
+    def change_theme(e):
+        page.theme_mode = 'light' if page.theme_mode == 'dark' else 'dark'
+        page.update()
+
+    page.add(
+        ft.Row(
+            [
+                ft.IconButton(ft.Icons.SUNNY, on_click = change_theme),
+                ft.Text('Программа для погоды')
+            ],
+            alignment = ft.MainAxisAlignment.CENTER
+        ),
+        ft.Row([user_data], alignment=ft.MainAxisAlignment.CENTER),
+        ft.Row([weather_data], alignment=ft.MainAxisAlignment.CENTER),
+        ft.Row([ft.ElevatedButton('Получить', on_click = get_info)], alignment=ft.MainAxisAlignment.CENTER)
+    )
+
+
+
+
+ft.app(target=main)
